9cd028bc96f855ff8351cb17f23af0b72834c862
jdog1994 <jesusdiedtosaveusfromoursins@gmail.com>
2015-04-09 17:25:26 -0400

    added page functionality

--- www/js/scripts.js
+++ www/js/scripts.js
@@ -3,3 +3,10 @@ document.addEventListener("deviceready",onDeviceReady,false);
 function onDeviceReady() {
     StatusBar.hide();
 }
+
+function CancelRegistration(){
+	var c = confirm("Are you sure you want to cancel?");
+	if(c) {
+		$.mobile.changePage("#home");
+	}	
+}
