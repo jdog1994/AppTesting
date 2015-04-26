b4c79606d1bfa1b7a8e49c24b8f46cbc5e7f11c8
jdog1994 <jesusdiedtosaveusfromoursins@gmail.com>
2015-04-26 14:34:53 -0400

    login form submission

--- www/js/scripts.js
+++ www/js/scripts.js
@@ -4,6 +4,24 @@ function onDeviceReady() {
     StatusBar.hide();
 }
 
+function submitForm(id) {
+	switch(id) {
+		case 'main_login':
+			login();
+			break;
+	}
+}
+
+function login() {
+	event.preventDefault();
+	var user = $('#email_main').val();
+	var password = $('#password_main').val();
+	alert(user);
+	$('#email_main').blur();
+	$('#password_main').blur();
+	return false;
+}
+
 function CancelRegistration(){
 	var c = confirm("Are you sure you want to cancel?");
 	if(c) {
